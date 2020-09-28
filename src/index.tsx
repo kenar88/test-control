c2f14acdb9d3490517414a9cdcc5d1bbdc0fc67a
Rustam Laykulov <roos1788@gmail.com>
2020-09-28 20:54:59 +0500

    fix: redux dev-tools removed

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger'
 
 import App from './App.js';
 import control from './reducer';
 import './styles/index.scss';
 
-const middlewareEnhancer  = compose(applyMiddleware(logger), composeWithDevTools());
+const middlewareEnhancer  = compose(applyMiddleware(logger));
 const roodReducer = combineReducers({
   control,
   form: formReducer,
