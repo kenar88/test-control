aef810ab8140c99039ba4926aa4b5cc27bb84963
Rustam Laykulov <roos1788@gmail.com>
2020-09-28 20:15:24 +0500

    Updates

---
+++ precache-manifest.9c58117ee90ad7bdcc1dfe0d1e2b2f31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69cb9177b915e1ccd0a258444574ce7e",
+    "url": "/test-control/index.html"
+  },
+  {
+    "revision": "f9f9850a458038f81ace",
+    "url": "/test-control/static/css/main.2afbb50c.chunk.css"
+  },
+  {
+    "revision": "f71a533e844f27718a63",
+    "url": "/test-control/static/js/2.0f41d71a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-control/static/js/2.0f41d71a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f9850a458038f81ace",
+    "url": "/test-control/static/js/main.782348fb.chunk.js"
+  },
+  {
+    "revision": "17907996d0865eff5d92",
+    "url": "/test-control/static/js/runtime-main.7f0c7e85.js"
+  }
+]);
